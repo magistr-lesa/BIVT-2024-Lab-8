43c3cb7b9780f4897a58d324063cc3d007d29b0b
magistr-lesa <kirillorinbaev@gmail.com>
2025-04-11 23:30:35 +0300

    Update Green_3.cs

--- Green_3.cs
+++ Green_3.cs
@@ -46,7 +46,7 @@ namespace Lab_8
         {
             return Output == null || Output.Length == 0
                 ? string.Empty
-                : string.Join("\r\n", Output);
+                : string.Join(Environment.NewLine, Output);
         }
     }
 }
